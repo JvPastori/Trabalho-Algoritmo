8c57cd94d80c9b3e803bcd3526bdf5c6f7c31cf4
João Vitor Pastori <124517985+JvPastori@users.noreply.github.com>
2023-04-26 18:54:07 -0300

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+main() {
+	
+	int x;
+	
+	printf("Digite o valor de x\n");
+	scanf("%i", &x);
+	
+	if (x% 3== 0) {
+		printf("%d e multiplo de 3\n");
+	
+	} else{
+		printf("%d nao e multiplo de 3\n", x);
+	}
+
+
+	
+	
+	
+}
